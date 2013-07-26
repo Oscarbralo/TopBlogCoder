1df62e368c3bdba84deda1a995db3c57f6d82fbc
Alethor <oscarbralo@outlook.com>
2013-07-26 17:04:13 +0200

    TopCoder problems

---
+++ SRMs/SRM180/DIV2/250/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DinkyFish a = new DinkyFish();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+    public class DinkyFish
+    {
+        public int monthsUntilCrowded(int tankVolume, int maleNum, int femaleNum)
+        {
+            int result = 0;
+            int total = tankVolume * 2;
+            int fish = maleNum + femaleNum;
+            while (fish <= total)
+            {
+                int tot = Math.Min(maleNum, femaleNum);
+                maleNum += tot;
+                femaleNum += tot;
+                fish = maleNum + femaleNum;
+                result++;
+            }
+            return result;
+        }
+    }
+}
