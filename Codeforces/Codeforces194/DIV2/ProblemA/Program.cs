1040548fe997f61e95e35d44722d8e2ccd2abe09
Alethor <oscarbralo@outlook.com>
2013-07-27 13:01:51 +0200

    new problems added

---
+++ Codeforces/Codeforces194/DIV2/ProblemA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A.Candy_Bags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            string[] people = new string[cases];
+            int j = cases * cases;
+            int x = 0;
+            for (int i = 0; i < (cases * cases) / 2; i++)
+            {
+                people[x] += (i + 1).ToString() + " " + j.ToString() + " ";
+                j--;
+                x++;
+                if (x == people.Length)
+                    x = 0;
+            }
+            for (int i = 0; i < people.Length; i++)
+                Console.WriteLine(people[i]);
+            Console.ReadLine();
+        }
+    }
+}
