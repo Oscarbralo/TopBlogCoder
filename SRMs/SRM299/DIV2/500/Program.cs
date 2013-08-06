6eac7f7496618c481bdff69ee06a34d5c12e5c69
Alethor <oscarbralo@outlook.com>
2013-08-06 17:44:52 +0200

    New problems added

---
+++ SRMs/SRM299/DIV2/500/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X x = new X();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+    public class X
+    {
+        public string x(string x)
+        {
+
+        }
+    }
+}
