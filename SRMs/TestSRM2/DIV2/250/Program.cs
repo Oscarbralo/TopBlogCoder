a10a9f25f94d248d2cd9880f064e4296601caba9
Alethor <oscarbralo@outlook.com>
2013-08-10 19:39:05 +0200

    new problems added

---
+++ SRMs/TestSRM2/DIV2/250/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SMSLanguage x = new SMSLanguage();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+    public class SMSLanguage
+    {
+        public string translate(string text)
+        {
+            string temp = text.ToLower().Replace(".", "").Replace(",", "").Replace("?", "").Replace("!","").Replace("and", "&").Replace("ate", "8").Replace("at", "@").Replace("you", "U");
+            return temp;
+        }
+    }
+}
