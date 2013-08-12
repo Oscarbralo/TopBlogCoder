05abee7ee7be2cc5ee20fc24beb7306f396ab0bf
Alethor <oscarbralo@outlook.com>
2013-08-12 07:07:35 +0200

    new problems added

---
+++ HackerRank/20_20HackAugust/MarkAndToys/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MarkAndToys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string[] t = Console.ReadLine().Split(' ');
+            int[] toys = new int[t.Length];
+            for (int i = 0; i < t.Length; i++)
+                toys[i] = int.Parse(t[i]);
+            Array.Sort(toys);
+            int result = 0;
+            long money = long.Parse(input[1]);
+            int toy = 0;
+            while (money > 0 && money - toys[toy] >= 0)
+            {
+                money -= toys[toy];
+                result++;
+                toy++;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
