c186e74d58342e46c413aef2904f3e6f2d9dadd8
Alethor <oscarbralo@outlook.com>
2013-08-14 22:40:19 +0200

    new problems added

---
+++ Codechef/EnormousInputTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enormous_Input_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cases = Console.ReadLine().Split(' ');
+            long res = 0;
+            for (int i = 0; i < int.Parse(cases[0]); i++)
+            {
+                long temp = long.Parse(Console.ReadLine());
+                if (temp % long.Parse(cases[1]) == 0)
+                    res++;
+            }
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
