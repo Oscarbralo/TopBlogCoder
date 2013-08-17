9578137c023495daacbf4cbff743d43a42e31a29
Alethor <oscarbralo@outlook.com>
2013-08-17 16:49:19 +0200

    new problems added

---
+++ Codeforces/Codeforces11/DIV2/ProblemA/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace A.Increasing_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int sum = int.Parse(input[1]);
+            string[] nums = Console.ReadLine().Split(' ');
+            int[] sequence = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+                sequence[i] = int.Parse(nums[i]);
+            int result = 0;
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (sequence[i] <= sequence[i - 1])
+                {
+                    int dif = sequence[i - 1] - sequence[i];
+                    if(dif == 0)
+                    {
+                        result++;
+                        sequence[i] += (sum);
+                        continue;
+                    }
+                    int times = dif / sum;
+                    sequence[i] += (sum * (times + 1));
+                    result += times + 1;
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
