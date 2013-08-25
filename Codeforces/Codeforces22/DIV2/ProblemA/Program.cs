82b5cd4da8d1629175771dba1e8db8627794466a
Alethor <oscarbralo@outlook.com>
2013-08-25 18:15:28 +0200

    new problems added

---
+++ Codeforces/Codeforces22/DIV2/ProblemA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A.Second_Order_Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = int.Parse(Console.ReadLine());
+            string[] temp = Console.ReadLine().Split(' ');
+            List<int> list = new List<int>();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (!list.Contains(int.Parse(temp[i])))
+                    list.Add(int.Parse(temp[i]));
+            }
+            int[] numbers = list.ToArray();
+            Array.Sort(numbers);
+            if (numbers.Length > 1)
+                Console.WriteLine(numbers[1]);
+            else
+                Console.WriteLine("NO");
+            Console.ReadLine();
+        }
+    }
+}
