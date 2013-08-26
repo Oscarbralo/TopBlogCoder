54616520209c7839dbbfa498c5015b64590a6d39
Alethor <oscarbralo@outlook.com>
2013-08-26 11:25:59 +0200

    new problems added

---
+++ Codeforces/Codeforces27/DIV2/ProblemA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A.Next_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int[] num = new int[input.Length];
+            for (int i = 0; i < num.Length; i++)
+                num[i] = int.Parse(input[i]);
+            Array.Sort(num);
+            int count = 1;
+            int res = 0;
+            while (num[res] == count)
+            {
+                res++;
+                count++;
+                if (res == num.Length)
+                    break;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
