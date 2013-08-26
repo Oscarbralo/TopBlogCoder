e35edc658b96e396768bd5b80676b90f7c25c621
Alethor <oscarbralo@outlook.com>
2013-08-26 14:14:50 +0200

    new problems added

---
+++ SRMs/SRM315/DIV2/500/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitsSum x = new DigitsSum();
+            Console.WriteLine(x.lastDigit(12345));
+            Console.ReadLine();
+        }
+    }
+
+    public class DigitsSum
+    {
+        public int lastDigit(int n)
+        {
+            if(n.ToString().Length == 1)
+                return n;
+            while (n.ToString().Length > 1)
+            {
+                string temp = n.ToString();
+                n = 0;
+                foreach (char t in temp)
+                    n += int.Parse(t.ToString());
+            }
+            return n;
+        }
+    }
+}
