d5cb2a51a2c5e995f5f2769008c8c14dc2a5b944
Alethor <oscarbralo@outlook.com>
2013-08-27 10:25:11 +0200

    new problem added

---
+++ Codeforces/Codeforces32/DIV2/ProblemA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A.Reconnaissance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string[] soldiers = Console.ReadLine().Split(' ');
+            int[] sol = new int[soldiers.Length];
+            int diff = int.Parse(input[1]);
+            for (int i = 0; i < soldiers.Length; i++)
+                sol[i] = int.Parse(soldiers[i]);
+            int result = 0;
+            for (int i = 0; i < sol.Length; i++)
+            {
+                for (int a = 0; a < sol.Length; a++)
+                {
+                    if (a == i)
+                        continue;
+                    if (Math.Abs(sol[i] - sol[a]) <= diff)
+                        result++;
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
