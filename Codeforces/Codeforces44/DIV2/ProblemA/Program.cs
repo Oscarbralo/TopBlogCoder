b0e7fa7086c7f462d5f56a8e67a5755d6ffb090a
Alethor <oscarbralo@outlook.com>
2013-08-31 17:57:24 +0200

    new problem added

---
+++ Codeforces/Codeforces44/DIV2/ProblemA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A.Indian_Summer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < cases; i++)
+            {
+                string[] temp = Console.ReadLine().Split(' ');
+                if (!list.Contains(temp[0] + " " + temp[1]))
+                {
+                    if (temp[0] != temp[1])
+                    {
+                        list.Add(temp[0] + " " + temp[1]);
+                    }
+                }
+            }
+            Console.WriteLine(list.Count);
+            Console.ReadLine();
+        }
+    }
+}
