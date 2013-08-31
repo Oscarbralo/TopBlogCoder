024581dcacba31908ed52de4bd8fbfdc3e7d03fa
Alethor <oscarbralo@outlook.com>
2013-08-31 11:07:30 +0200

    new problem added

---
+++ Codeforces/Codeforces41/DIV2/ProblemA/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace A.Translation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine().ToLower();
+            string input2 = Console.ReadLine().ToLower();
+            bool correct = true;
+            if (input1.Length != input2.Length)
+                Console.WriteLine("NO");
+            else
+            {
+                int last = input2.Length - 1;
+                for (int i = 0; i < input1.Length; i++)
+                {
+                    if (input1[i] == 's')
+                    {
+                        if (input2[last] == 't')
+                        {
+                            last--;
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        if (input1[i] != input2[last])
+                        {
+                            correct = false;
+                            Console.WriteLine("NO");
+                            break;
+                        }
+                    }
+                    last--;
+                }
+            }
+            if(correct)
+                Console.WriteLine("YES");
+            Console.ReadLine();
+        }
+    }
+}
