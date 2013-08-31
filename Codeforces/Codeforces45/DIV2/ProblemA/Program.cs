848e4f0105674a0422d34156f10c08e4112d2146
Alethor <oscarbralo@outlook.com>
2013-08-31 18:59:53 +0200

    new problem added

---
+++ Codeforces/Codeforces45/DIV2/ProblemA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace A.Codecraft_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actualMonth = Console.ReadLine();
+            int numberMonths = int.Parse(Console.ReadLine());
+            List<string> months = new List<string> { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            int actual = (months.IndexOf(actualMonth) + numberMonths) % 12;
+            Console.WriteLine(months[actual]);
+            Console.ReadLine();
+        }
+    }
+}
