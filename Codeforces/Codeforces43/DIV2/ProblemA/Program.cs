960d03200d8de3e3e2efd83e699a35a3886b7bd3
Alethor <oscarbralo@outlook.com>
2013-08-31 17:29:16 +0200

    new problem added

---
+++ Codeforces/Codeforces43/DIV2/ProblemA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A.Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goals = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            List<int> g = new List<int>();
+            for (int i = 0; i < goals; i++)
+            {
+                string temp = Console.ReadLine();
+                if (!list.Contains(temp))
+                {
+                    list.Add(temp);
+                    g.Add(1);
+                }
+                else
+                    g[list.IndexOf(temp)]++;
+            }
+            if (list.Count == 1)
+                Console.WriteLine(list[0]);
+            else
+            {
+                if (g[0] > g[1])
+                    Console.WriteLine(list[0]);
+                else
+                    Console.WriteLine(list[1]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
