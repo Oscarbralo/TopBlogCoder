ae8af6b76a5bdd4f1205708f6626b28e602c4ef2
Alethor <oscarbralo@outlook.com>
2013-09-01 08:51:39 +0200

    new problem added

---
+++ SRMs/SRM324/DIV2/500/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromeDecoding x = new PalindromeDecoding();
+            int[] a = { 2, 3, 1, 7 };
+            int[] b = { 1, 1, 2, 2 };
+            Console.WriteLine(x.decode("Misip", a, b));
+            Console.ReadLine();
+        }
+    }
+
+    public class PalindromeDecoding
+    {
+        public string decode(string code, int[] position, int[] length)
+        {
+            StringBuilder Ccode = new StringBuilder(code);
+            if (position.Length == 0)
+                return code;
+            for (int i = 0; i < position.Length; i++)
+            {
+                string temp = Ccode.ToString().Substring(position[i], length[i]);
+                StringBuilder sb = new StringBuilder();
+                for (int a = temp.Length - 1; a >= 0;a--)
+                    sb.Append(temp[a]);
+                Ccode = Ccode.Insert(position[i]+length[i], sb.ToString());
+            }
+            return Ccode.ToString();
+        }
+    }
+}
