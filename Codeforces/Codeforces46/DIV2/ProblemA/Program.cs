280dc1132c439ebad49e1fa96a6a0ebe0a6e6a5b
Alethor <oscarbralo@outlook.com>
2013-09-01 08:09:59 +0200

    new problem added

---
+++ Codeforces/Codeforces46/DIV2/ProblemA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace A.Ball_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            int actual = 0;
+            int count = 1;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < cases - 1; i++)
+            {
+                actual += count;
+                if (actual >= cases)
+                    actual %= cases;
+                sb.Append((actual + 1).ToString() + " ");
+                count++;
+            }
+            sb = sb.Remove(sb.Length - 1,1);
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
