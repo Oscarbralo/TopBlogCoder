06af257b3677fefa543997805eb9d20c40be3900
Alethor <oscarbralo@outlook.com>
2013-09-01 15:59:25 +0200

    new problem added

---
+++ Codeforces/Codeforces49/DIV2/ProblemA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A.Sleuth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower().Replace(" ","").Replace(",","").Replace(".","").Replace("?","").Replace("!","");
+            string correct = "aeiouy";
+            if(correct.Contains(input[input.Length - 1].ToString()))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            Console.ReadLine();
+        }
+    }
+}
