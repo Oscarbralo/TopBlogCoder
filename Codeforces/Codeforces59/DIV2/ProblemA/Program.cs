4731fd5da2600c03b816d2dd2087a60fd7578b0a
Alethor <oscarbralo@outlook.com>
2013-09-05 17:20:55 +0200

    new problem added

---
+++ Codeforces/Codeforces59/DIV2/ProblemA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace A.Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Regex lower = new Regex("[a-z]");
+            Regex upper = new Regex("[A-Z]");
+            int low = lower.Matches(s).Count;
+            int up = upper.Matches(s).Count;
+            if(up > low)
+                Console.WriteLine(s.ToUpper());
+            else
+                Console.WriteLine(s.ToLower());
+            Console.ReadLine();
+        }
+    }
+}
