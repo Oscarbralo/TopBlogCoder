e176e709852a1d54f6c256a504e5a1bf64fb290c
Alethor <oscarbralo@outlook.com>
2013-09-05 16:30:37 +0200

    new problem added

---
+++ Codeforces/Codeforces56/DIV2/ProblemA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace A.Bar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alcohol = { "ABSINTH", "BEER", "BRANDY", "CHAMPAGNE", "GIN", "RUM", "SAKE", "TEQUILA", "VODKA", "WHISKEY", "WINE" };
+            int cases = int.Parse(Console.ReadLine());
+            int result = 0;
+            Regex r = new Regex(@"\d");
+            for (int i = 0; i < cases; i++)
+            {
+                string temp = Console.ReadLine();
+                if (r.IsMatch(temp))
+                {
+                    if (int.Parse(temp) < 18)
+                        result++;
+                }
+                else
+                {
+                    if (alcohol.Contains(temp))
+                        result++;
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
