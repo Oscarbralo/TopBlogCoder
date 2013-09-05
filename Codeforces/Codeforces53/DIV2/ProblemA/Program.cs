df07f20ab34c16a3a8df2692008234b985554d24
Alethor <oscarbralo@outlook.com>
2013-09-05 14:41:01 +0200

    new problem added

---
+++ Codeforces/Codeforces53/DIV2/ProblemA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A.Autocomplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int cases = int.Parse(Console.ReadLine());
+            string[] input = new string[cases];
+            for (int i = 0; i < cases; i++)
+                input[i] = Console.ReadLine();
+            Array.Sort(input);
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Contains(word))
+                {
+                    result = input[i];
+                    break;
+                }
+            }
+            if (result == "")
+                result = word;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
