ed01ac62558bbf51fd41c7334efe55a6364d2fd0
Alethor <oscarbralo@outlook.com>
2013-09-05 18:04:43 +0200

    new problem added

---
+++ Codeforces/Codeforces61/DIV2/ProblemA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace A.Ultra_Fast_Mathematician
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] == s2[i])
+                    sb.Append("0");
+                else
+                    sb.Append("1");
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
