59018d144d48d25a42c64493b4933b1d2f82c0c4
Oscarbralo <oscarbralo@outlook.com>
2013-10-01 23:21:29 +0200

    new problems added

---
+++ HackerRank/HackerrankBuildingASmartIDE/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BuildingASmartID
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            bool x = true;
+            for (int a = 0; a < 15; a++)
+            {
+                string temp = Console.ReadLine();
+                if (temp.Contains("java"))
+                {
+                    Console.WriteLine("Java");
+                    x = false;
+                    break;
+                }
+                else if (temp.Contains("stdio"))
+                {
+                    Console.WriteLine("C");
+                    x = false;
+                    break;
+                }
+            }
+            if (x)
+                Console.WriteLine("Python");
+        }
+    }
+}
