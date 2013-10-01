c26c96ebeff69387d1755d3cbcf9b463aa21a581
Oscarbralo <oscarbralo@outlook.com>
2013-10-01 22:52:04 +0200

    new problems added

---
+++ HackerRank/HackerrankMarkAndToys/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MarkAndToys
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string[] numbers = Console.ReadLine().Split(' ');
+            int[] prices = new int[numbers.Length];
+            for (int a = 0; a < numbers.Length; a++)
+                prices[a] = int.Parse(numbers[a]);
+            Array.Sort(prices);
+            int money = int.Parse(input[1]);
+            int result = 0;
+            for (int b = 0; b < prices.Length; b++)
+            {
+                if (money == 0)
+                    break;
+                if (money < 0)
+                {
+                    result--;
+                    break;
+                }
+                money -= prices[b];
+                result++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
