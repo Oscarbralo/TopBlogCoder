53c88eeeb663b41d34efc89c57477a97fcb3739d
Oscarbralo <oscarbralo@outlook.com>
2013-10-01 09:12:45 +0200

    new problem added

---
+++ HackerRank/HackerrankIntroToTutorialChallenges/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntroToTutorialChallenges
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            string number = Console.ReadLine();
+            int length = int.Parse(Console.ReadLine());
+            List<string> array = Console.ReadLine().Split(' ').ToList();
+            int result = array.IndexOf(number);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
