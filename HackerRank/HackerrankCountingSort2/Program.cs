3965c6fc9620a9e83583ad0197bee983ee563f32
Oscarbralo <oscarbralo@outlook.com>
2013-10-02 11:19:00 +0200

    new problem added

---
+++ HackerRank/HackerrankCountingSort2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CountingSort1
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            int[] res = new int[cases];
+            string s = Console.ReadLine();
+            string[] input = s.Split(' ');
+            for (int a = 0; a < cases; a++)
+                res[a] = int.Parse(input[a]);
+            Array.Sort(res);
+            Console.WriteLine(string.Join(" ", res));
+        }
+    }
+}
