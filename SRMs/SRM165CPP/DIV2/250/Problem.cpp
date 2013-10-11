817baa3625b36b4ae150e17f75c5541b35b18342
Oscarbralo <oscarbralo@outlook.com>
2013-10-11 23:20:29 +0200

    new problem added

---
+++ SRMs/SRM165CPP/DIV2/250/Problem.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Template {
+public:
+	int template() {
+		return 0
+	}
+};
+
+int main()
+{
+	return 0;
+}
+
