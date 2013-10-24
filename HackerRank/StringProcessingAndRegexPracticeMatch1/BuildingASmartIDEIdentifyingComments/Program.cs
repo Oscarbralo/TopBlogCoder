484da1adcae1e38a53d58c10b2fcf073d2896dee
Oscarbralo <oscarbralo@outlook.com>
2013-10-24 13:12:43 +0200

    new contest

---
+++ HackerRank/StringProcessingAndRegexPracticeMatch1/BuildingASmartIDEIdentifyingComments/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BuildingASmartIDEIdentifyingComment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> result = new List<string>();
+            bool x = false;
+            Regex r = new Regex(@"//");
+            Regex r1 = new Regex(@"beginline");
+            Regex r2 = new Regex(@"endline");
+            for (int a = 0; a < 200; a++)
+            {
+                string line = Console.ReadLine();
+                if (line == null)
+                    break;
+                line = line.Replace(@"*/", "endline");
+                line = line.Replace(@"/*", "beginline");
+                if (r.IsMatch(line))
+                {
+                    result.Add(line.Substring(r.Match(line).Index, line.Length - r.Match(line).Index));
+                    continue;
+                }
+                if (r1.IsMatch(line))
+                {
+                    x = true;
+                    if (r2.IsMatch(line))
+                    {
+                        line = line.Replace("endline", @"*/");
+                        x = false;
+                    }
+                    line = line.Replace("beginline", @"/*");
+                    result.Add(line);
+                    continue;
+                }
+                if (x)
+                {
+                    if (r2.IsMatch(line))
+                    {
+                        line = line.Replace("endline", @"*/");
+                        result.Add(line.Substring(0, line.Length - r2.Match(line).Index));
+                        x = false;
+                    }
+                    else
+                        result.Add(line);
+                }
+            }
+            for (int i = 0; i < result.Count; i++)
+                Console.WriteLine(result[i]);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ HackerRank/StringProcessingAndRegexPracticeMatch1/DetectingValidLatitudeAndLongitudePairs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DetectingValidLatitudeAndLongitudePairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                string temp = Console.ReadLine();
+                temp = temp.Replace(")", "");
+                temp = temp.Replace("(", "");
+                temp = temp.Replace("+", "");
+                temp = temp.Replace("-", "");
+                string[] numbers = temp.Split(',');
+                if (numbers[0][numbers[0].Length - 1] == '.' || numbers[1][numbers[1].Length - 1] == '.')
+                {
+                    Console.WriteLine("Invalid");
+                    continue;
+                }
+                string[] xNumber = new string[2];
+                string[] yNumber = new string[2];
+                int x = 0;
+                int y = 0;
+                if (numbers[0].Contains("."))
+                {
+                    xNumber = numbers[0].Split('.');
+                    if (int.Parse(xNumber[0]) == 90 && int.Parse(xNumber[1]) > 0 || xNumber[0].Length > 2)
+                    {
+                        Console.WriteLine("Invalid");
+                        continue;
+                    }
+                    x = int.Parse(xNumber[0]);
+                }
+                else
+                    x = int.Parse(numbers[0]);
+                if (numbers[1].Contains("."))
+                {
+                    yNumber = numbers[1].Split('.');
+                    if (int.Parse(yNumber[0]) == 180 && int.Parse(yNumber[1]) > 0 || yNumber[0].Length > 4)
+                    {
+                        Console.WriteLine("Invalid");
+                        continue;
+                    }
+                    y = int.Parse(yNumber[0]);
+                }
+                else
+                    y = int.Parse(numbers[1]);
+                if (x <= 90 && y <= 180)
+                    Console.WriteLine("Valid");
+                else
+                    Console.WriteLine("Invalid");
+            }
+            Console.ReadLine();
+        }
+    }
+}
