48be87a92b339fbaf30696a50cde7f669b903fb2
Oscarbralo <oscarbralo@outlook.com>
2013-10-27 21:00:29 +0100

    new problem added

---
+++ HackerRank/20_20HackOctober/AngryChildren/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AngryChildren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            int max = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+                list.Add(int.Parse(Console.ReadLine()));
+            int[] temp = list.ToArray();
+            Array.Sort(temp);
+            int min = 100000000;
+            for (int a = 0; a < temp.Length - max; a++)
+            {
+                int t = temp[a + (max - 1)] - temp[a];
+                if (t < min)
+                    min = t;
+            }
+            Console.WriteLine(min);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HackerRank/20_20HackOctober/ChocolateFeast/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ChocolateFeast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int res = 0;
+                string[] temp = Console.ReadLine().Split(' ');
+                int candies = int.Parse(temp[0]) / int.Parse(temp[1]);
+                res += candies;
+                int wrapper = res;
+                while (wrapper >= int.Parse(temp[2]))
+                {
+                    int extra = wrapper / int.Parse(temp[2]);
+                    wrapper %= int.Parse(temp[2]);
+                    res += extra;
+                    wrapper += extra;
+                }
+                Console.WriteLine(res);
+            }
+            Console.ReadLine();
+        }
+    }
+}
