99a5674b890963c86c287db7be90669cbe5d6a4e
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-11-11 17:48:46 +0100

    new problem added

---
+++ Codeforces/Codeforces78/DIV2/ProblemA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Haiku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(@"[aeiou]");
+            int[] haiku = new int[3];
+            for (int i = 0; i < 3; i++)
+                haiku[i] = r.Matches(Console.ReadLine()).Count;
+            if(haiku[0] == 5 && haiku[1] == 7 && haiku[2] == 5)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            Console.ReadLine();
+        }
+    }
+}
