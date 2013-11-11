64c2a10b432436c9bde081f64977efc72d87fc18
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-11-11 12:06:01 +0100

    new problem added

--- Codeforces/Codeforces75/DIV2/ProblemA/Program.cs
+++ Codeforces/Codeforces75/DIV2/ProblemA/Program.cs
@@ -8,11 +8,9 @@ namespace LifeWithoutZeros
         {
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
-            int c = a + b;
             int a1 = int.Parse(a.ToString().Replace("0", ""));
             int b1 = int.Parse(b.ToString().Replace("0", ""));
-            int c1 = a1 + b1;
-            if(c.ToString().Replace("0", "") == c1.ToString())
+            if((a + b).ToString().Replace("0", "") == (a1 + b1).ToString())
                 Console.WriteLine("YES");
             else
                 Console.WriteLine("NO");
