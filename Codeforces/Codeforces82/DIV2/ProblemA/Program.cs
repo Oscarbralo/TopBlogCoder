35ddbe19999d27e9bba9ac43b9ef0dee8abe4364
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-11-13 08:32:27 +0100

    new problem added

---
+++ Codeforces/Codeforces82/DIV2/ProblemA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DoubleCola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int db = 1;
+            int index = 0;
+            string[] winner = { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+            while (n >= 1)
+            {
+                index = 0;
+                for (int i = 0; i < winner.Length; i++)
+                {
+                    n -= db;
+                    if (n <= 0) { break; }
+                    index++;
+                }
+                db *= 2;
+            }
+            Console.WriteLine(winner[index]);
+            Console.ReadLine();
+        }
+    }
+}
