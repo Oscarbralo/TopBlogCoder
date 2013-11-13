8214b31e1aa4112229e9c739ecd44ce04c6b7654
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-11-13 11:04:50 +0100

    new problem added

---
+++ Codeforces/Codeforces84/DIV2/ProblemA/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ToyArmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine((n / 2) * 3);
+            Console.ReadLine();
+        }
+    }
+}
