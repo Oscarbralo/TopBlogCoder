eb699d6c9fec59265b498a9128a1c3166f53bcfe
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-11-13 11:07:36 +0100

    new problem added

---
+++ Codeforces/Codeforces84Cpp/DIV2/ProblemA/Problem.cpp
@@ -0,0 +1,34 @@
+// TCTemplate.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	long n;
+	cin >> n;
+	cout << (n / 2) * 3 << endl;
+	return 0;
+}
+
