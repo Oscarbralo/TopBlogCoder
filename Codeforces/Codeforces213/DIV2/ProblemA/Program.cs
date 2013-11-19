40cbfd7e29125b65f60911f9e679afcc3950e643
Oscarbralo <oscarbralo@outlook.com>
2013-11-19 19:33:02 +0100

    new problem added

---
+++ Codeforces/Codeforces213/DIV2/ProblemA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProblemA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int cases = int.Parse(input[0]);
+            int maxNumber = int.Parse(input[1]);
+            int result = 0;
+            for (int i = 0; i < cases; i++)
+            {
+                string n = Console.ReadLine();
+                bool finish = false;
+                for (int a = 0; a <= maxNumber; a++)
+                {
+                    if (!n.Contains(a.ToString()))
+                    {
+                        finish = true;
+                        break;
+                    }
+                }
+                if (!finish)
+                    result++;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
