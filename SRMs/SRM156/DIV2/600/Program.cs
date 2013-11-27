e909cf3ba69266c41715ec4858fa9af28bf1ef8f
Oscarbralo <oscarbralo@outlook.com>
2013-11-27 21:26:18 +0100

    new problem added

---
+++ SRMs/SRM156/DIV2/600/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _600
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BombSweeper x = new BombSweeper();
+            string[] a = {".....",
+ ".....",
+ "..B..",
+ ".....",
+ "....."};
+            Console.WriteLine(x.winPercentage(a));
+            Console.ReadLine();
+        }
+    }
+
+    public class BombSweeper
+    {
+        public double winPercentage(string[] board)
+        {
+            string[] newBoard = new string[board.Length + 2];
+            newBoard[0] = "." + board[0].Replace("B", ".") + ".";
+            for (int i = 0; i < board.Length; i++)
+                newBoard[i + 1] = "." + board[i] + ".";
+            newBoard[newBoard.Length - 1] = newBoard[0];
+            int result = 0;
+            int bombs = 0;
+            for (int a = 1; a < newBoard.Length - 1; a++)
+            {
+                for (int b = 1; b < newBoard[a].Length - 1; b++)
+                {
+                    int count = 0;
+                    if (newBoard[a][b] == 'B')
+                        bombs++;
+                    else
+                    {
+                        if (newBoard[a - 1][b] == '.')
+                            count++;
+                        if (newBoard[a - 1][b - 1] == '.')
+                            count++;
+                        if (newBoard[a][b - 1] == '.')
+                            count++;
+                        if (newBoard[a + 1][b - 1] == '.')
+                            count++;
+                        if (newBoard[a + 1][b] == '.')
+                            count++;
+                        if (newBoard[a + 1][b + 1] == '.')
+                            count++;
+                        if (newBoard[a][b + 1] == '.')
+                            count++;
+                        if (newBoard[a - 1][b + 1] == '.')
+                            count++;
+                        if (count == 8)
+                            result++;
+                    }
+                }
+            }
+            double res = double.Parse(result.ToString()) / (double.Parse(result.ToString()) + double.Parse(bombs.ToString()));
+            double r = res * 100.0;
+            return r;
+        }
+    }
+}
