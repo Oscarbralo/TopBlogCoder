a1908f651cf079dccd04f2837116b1a51cc26f6e
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-12-04 09:16:07 +0100

    new problem added

---
+++ Codechef/PackagingCupcakes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PackagingCupcakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int res = 0;
+                int n = int.Parse(Console.ReadLine());
+                if (n == 2)
+                    res = 2;
+                else
+                    res = n - (n % (( n / 2) + 1));
+                Console.WriteLine(res);
+            }
+            Console.ReadLine();
+        }
+    }
+}
