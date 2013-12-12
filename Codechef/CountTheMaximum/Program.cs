f65db64b1ea0ef1fbf86c9774e80d6b948f92bae
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-12-12 08:34:29 +0100

    new problem added

---
+++ Codechef/CountTheMaximum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CountTheMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int result = 0;
+                int resultTimes = 0;
+                int n = int.Parse(Console.ReadLine());
+                int[] v = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).OrderBy(x => x).ToArray<int>();
+                for (int a = 0; a < v.Length; a++)
+                {
+                    int count = v.Select(x => x).Where(x => x == v[a]).Count();
+                    if (count > resultTimes)
+                    {
+                        resultTimes = count;
+                        result = v[a];
+                    }
+                }
+                Console.WriteLine(result + " " + resultTimes);
+            }
+            Console.ReadLine();
+        }
+    }
+}
