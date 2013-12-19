abf44205e249e1c7a0c9a35b4bf343684c1aea0b
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-12-19 11:03:13 +0100

    new problem added

---
+++ Codeforces/Codeforces104/DIV2/ProblemA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if(n <= 10 || n > 21)
+                Console.WriteLine(0);
+            else if (n < 20)
+                Console.WriteLine(4);
+            else if (n == 20)
+                Console.WriteLine(15);
+            else
+                Console.WriteLine(19);
+        }
+    }
+}
