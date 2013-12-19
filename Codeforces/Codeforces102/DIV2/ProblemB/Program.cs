1685ad31aea8d917a37f982679d6999928ef6d4e
U-EUROTAX_NETBIOS\oscarbralo <oscarbralo@PC-OSCARB.eurotax.dns.com>
2013-12-19 08:30:40 +0100

    new problem added

---
+++ Codeforces/Codeforces102/DIV2/ProblemB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int temp = 11;
+            string n = Console.ReadLine();
+            while (temp.ToString().Length != 1 && n.Length > 1)
+            {
+                temp = n.Sum(x => int.Parse(x.ToString()));
+                n = temp.ToString();
+                result++;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
