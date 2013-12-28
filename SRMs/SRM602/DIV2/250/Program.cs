e13028f7d35fb029332c773a0a529932b0eb997e
Oscarbralo <oscarbralo@outlook.com>
2013-12-28 20:45:49 +0100

    new problem added

---
+++ SRMs/SRM602/DIV2/250/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X x = new X();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+    public class TypoCoderDiv2
+    {
+        public int count(int[] rating)
+        {
+            int current = 500;
+            bool low = true;
+            int res = 0;
+            for (int i = 0; i < rating.Length; i++)
+            {
+                current = rating[i];
+                if (current >= 1200 && low == true)
+                {
+                    res++;
+                    low = false;
+                }
+                else if (current < 1200 && low == false)
+                {
+                    res++;
+                    low = true;
+                }
+            }
+            return res;
+        }
+    }
+}
