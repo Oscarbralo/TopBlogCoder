c08c149a326a911e3c528ac1287116d755067151
Oscarbralo <oscarbralo@outlook.com>
2013-12-28 09:51:22 +0100

    new problem added

---
+++ Codeforces/Codeforces106/DIV2/ProblemA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string suit = Console.ReadLine();
+            string[] cards = Console.ReadLine().Split(' ');
+            string deck = "6789TJQKA";
+            if (cards[0][1].ToString() == suit && cards[1][1].ToString() != suit)
+                Console.WriteLine("YES");
+            else if(cards[0][1] == cards[1][1] && deck.IndexOf(cards[0][0].ToString()) > deck.IndexOf(cards[1][0].ToString()))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            Console.ReadLine();
+        }
+    }
+}
