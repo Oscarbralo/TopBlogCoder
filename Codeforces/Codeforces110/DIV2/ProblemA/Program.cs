5f8bb01e31c94bfb3c724fe72a5b6d9579d11af1
Oscarbralo <oscarbralo@outlook.com>
2014-01-02 12:39:26 +0100

    new problem added

---
+++ Codeforces/Codeforces110/DIV2/ProblemA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NearlyLuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex r = new Regex("[47]");
+            int n = r.Matches(input).Count;
+            Console.WriteLine((n == 4 || n == 7) ? "YES" : "NO");
+            Console.ReadLine();
+        }
+    }
+}
