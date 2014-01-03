8b318a4a4217d25eefae86fb81846795125f4dd2
Oscarbralo <oscarbralo@outlook.com>
2014-01-03 09:59:09 +0100

    new problem added

---
+++ Codeforces/Codeforces112/DIV2/ProblemA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PetyaAndString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Compare(Console.ReadLine().ToLower(), Console.ReadLine().ToLower()));
+            Console.ReadLine();
+        }
+    }
+}
