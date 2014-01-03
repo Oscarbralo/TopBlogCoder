589f6e9e96e2a3630c6165bd23f7ca4707b07c2c
Oscarbralo <oscarbralo@outlook.com>
2014-01-03 23:13:26 +0100

    new problem added

---
+++ Codeforces/Codeforces116/DIV2/ProblemA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Tram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            int peopleMax = 0;
+            int max = 0;
+            for (int i = 0; i < cases; i++)
+            {
+                int[] temp = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+                peopleMax -= temp[0];
+                peopleMax += temp[1];
+                max = Math.Max(peopleMax, max);
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
