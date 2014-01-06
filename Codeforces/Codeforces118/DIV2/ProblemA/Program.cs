bb2a7bf5c1806360f19507a172440dfee328fb5f
Oscarbralo <oscarbralo@outlook.com>
2014-01-06 22:05:35 +0100

    new problem added

---
+++ Codeforces/Codeforces118/DIV2/ProblemA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace StringTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            string[] res = Regex.Replace(text, "[aeiouy]", "").Select(x => x.ToString()).ToArray<string>();
+            Console.WriteLine( "." + string.Join(".", res));
+            Console.ReadLine();
+        }
+    }
+}
