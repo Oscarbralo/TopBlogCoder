bceb12b450c4fd023b7bb6279472230c1178451e
Oscarbralo <oscarbralo@outlook.com>
2014-01-07 21:18:34 +0100

    new problem added

---
+++ Codeforces/Codeforces122/DIV2/ProblemA/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace AlmostLucky
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Lucky l = new Lucky();
+            int n = int.Parse(Console.ReadLine());
+            if (l.isLucky(n))
+                Console.WriteLine("YES");
+            else
+            {
+                bool finish = false;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (l.isLucky(i) && n % i == 0)
+                    {
+                        finish = true;
+                        break;
+                    }
+                }
+                Console.WriteLine((finish) ? "YES" : "NO");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Lucky
+    {
+        public bool isLucky(int n)
+        {
+            int sum = n.ToString().Select(x => x).Where(x => x == '4').Count();
+            sum += n.ToString().Select(x => x).Where(x => x == '7').Count();
+            if (sum == n.ToString().Length)
+                return true;
+            else
+                return false;
+        }
+    }
+}
