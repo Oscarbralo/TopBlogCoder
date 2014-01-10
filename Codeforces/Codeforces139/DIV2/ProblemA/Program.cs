b6a34cbc62f12a379cb75cca2461568a7e4086cc
Oscarbralo <oscarbralo@outlook.com>
2014-01-11 00:06:18 +0100

    new problem added

---
+++ Codeforces/Codeforces139/DIV2/ProblemA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace PetrAndBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPages = int.Parse(Console.ReadLine());
+            int[] pages = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int currentPage = -1;
+            while (totalPages > 0)
+            {
+                currentPage = (currentPage + 1) % 7;
+                totalPages -= pages[currentPage];
+            }
+            Console.WriteLine(++currentPage);
+            Console.ReadLine();
+        }
+    }
+}
