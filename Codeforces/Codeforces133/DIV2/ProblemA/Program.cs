477f02e0195203a9066fee2c87d9c4dda5fcfb5e
Oscarbralo <oscarbralo@outlook.com>
2014-01-10 19:35:51 +0100

    new problem added

---
+++ Codeforces/Codeforces133/DIV2/ProblemA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HQ9_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex r = new Regex(@"[HQ9]");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()) ? "YES" : "NO");
+            Console.ReadLine();
+        }
+    }
+}
