5df93948a8d84a586ac8cc769a27ee5558459844
Oscarbralo <oscarbralo@outlook.com>
2014-01-15 22:48:53 +0100

    new problem added

---
+++ Codeforces/Codeforces149/DIV2/ProblemA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace BusinessTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cm = int.Parse(Console.ReadLine());
+            int[] months = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            Array.Sort(months);
+            int result = 0;
+            if (months.Sum() < cm)
+                Console.WriteLine(-1);
+            else
+            {
+                int x = months.Length - 1;
+                while (cm > 0)
+                {
+                    cm -= months[x];
+                    x--;
+                    result++;
+                }
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
