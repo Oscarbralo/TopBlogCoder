00218cad2a93ba30533d08ac20d7453be58fc1a0
Oscarbralo <oscarbralo@outlook.com>
2014-01-17 22:26:40 +0100

    new problem added

---
+++ Codeforces/Codeforces158/DIV2/ProblemA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace NextRound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cases = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int[] input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            Console.WriteLine(input.Select(x => x).Where(x => x >= input[cases[1] - 1]).Count());
+            Console.ReadLine();
+        }
+    }
+}
