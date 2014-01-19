9fc67d5f0974a7e879b5c080fd55dbde348f8bc7
Oscarbralo <oscarbralo@outlook.com>
2014-01-19 16:25:24 +0100

    new problem added

---
+++ HackerEarth/PracticePlacementChallenge1/PalindromeCount/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace PalindromeCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            List<string> list = new List<string>();
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int x = 1;
+                while (x <= text.Length - i)
+                {
+                    list.Add(text.Substring(i, x));
+                    x++;
+                }
+            }
+            list.Add(text[text.Length - 1].ToString());
+            int result = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length == 1)
+                    result++;
+                else
+                {
+                    bool finish = true;
+                    int end = list[i].Length - 1;
+                    for (int a = 0; a < list[i].Length / 2; a++)
+                    {
+                        if (list[i][a] != list[i][end])
+                        {
+                            finish = false;
+                            break;
+                        }
+                        end--;
+                    }
+                    if (finish)
+                        result++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
