07be1bf275accce269838cd7e5fefff41908e2c9
Oscarbralo <oscarbralo@outlook.com>
2014-01-19 14:23:38 +0100

    new problem added

---
+++ Codeforces/Codeforces160/DIV2/ProblemA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Twins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            IEnumerable<int> coinsUnsorted = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            List<int> list = coinsUnsorted.OrderByDescending(x => x).ToList<int>();
+            //if (list.Count == 1)
+            //    Console.WriteLine(1);
+            //else
+            //{
+                for (int i = 1; i <= list.Count; i++)
+                {
+                    int res = (list.Select(x => x).Take(i).Sum() > list.Skip(i).Select(x => x).Sum()) ? i : -1;
+                    if (res != -1)
+                    {
+                        Console.WriteLine(res);
+                        break;
+                    }
+                }
+            
+            Console.ReadLine();
+        }
+    }
+}
