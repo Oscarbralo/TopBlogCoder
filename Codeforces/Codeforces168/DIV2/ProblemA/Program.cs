b168bed08677cdfbe7905625fa2b53d2eb4b1f87
Oscarbralo <oscarbralo@outlook.com>
2014-01-23 23:02:15 +0100

    new problem added

---
+++ Codeforces/Codeforces168/DIV2/ProblemA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace WizardsAndDemostration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split(' ').Select(x => double.Parse(x)).ToArray<double>();
+            double temp = (Math.Ceiling(((input[2] * input[0]) / 100)) - input[1]);
+            Console.WriteLine(temp < 0 ? 0 : temp);
+            Console.ReadLine();
+        }
+    }
+}
