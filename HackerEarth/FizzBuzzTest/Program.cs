8084edd43067bda8069555b4dc6c19a2b157ff8c
Oscarbralo <oscarbralo@outlook.com>
2014-01-24 21:48:28 +0100

    new problem added

---
+++ HackerEarth/FizzBuzzTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine((i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : i.ToString());
+        }
+    }
+}
