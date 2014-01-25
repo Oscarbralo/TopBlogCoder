909fb8db7a02742956cd38dd5b1b7edde4fb85b2
Oscarbralo <oscarbralo@outlook.com>
2014-01-25 13:58:06 +0100

    new problem added

---
+++ SRMs/SRM184/DIV2/500/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BagOfHolding x = new BagOfHolding();
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(x.oddsReachable(a, 4));
+            Console.ReadLine();
+        }
+    }
+
+    public class BagOfHolding
+    {
+        public double oddsReachable(int[] sizes, int item)
+	    {
+            return 1 / double.Parse(sizes.Select(x => x).Where(x => x >= sizes[item]).ToArray<int>().Length.ToString());
+	    }
+    }
+}
