80b3a3fa8c6d748702835f0737c91fc1d24b004a
Oscarbralo <oscarbralo@outlook.com>
2014-01-25 16:01:36 +0100

    new problem added

---
+++ SRMs/SRM186/DIV2/500/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordWrap x = new WordWrap();
+            string[] a = {"now is the time for all good men",
+ "to come to the aid of their country"};
+            Console.WriteLine(x.justify(a, 20));
+            Console.ReadLine();
+        }
+    }
+
+    public class WordWrap
+    {
+        public string[] justify(string[] lines, int columnWidth)
+        {
+            string total = string.Join(" ", lines);
+            string[] words = total.Split(' ');
+            List<string> list = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                while (i != words.Length && sb.Length + (words[i].Length + 1) <= columnWidth + 1)
+                {
+                    sb.Append(words[i] + " ");
+                    i++;
+                }
+                list.Add((sb[sb.ToString().Length - 1] == ' ') ? sb.ToString().Remove(sb.ToString().Length - 1, 1) : sb.ToString());
+                i--;
+            }
+            return list.ToArray();
+        }
+    }
+}
+
