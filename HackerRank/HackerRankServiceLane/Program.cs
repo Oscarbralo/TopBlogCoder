979738b0a54d70504fc3389a8810ce298a82dce2
Oscarbralo <oscarbralo@outlook.com>
2014-02-03 19:42:57 +0100

    New algorithms added

---
+++ HackerRank/HackerRankServiceLane/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+        int[] road = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+        for (int i = 0; i < input[1]; i++)
+        {
+            int[] query = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            Console.WriteLine(road.Skip(query[0]).Take((query[1] - query[0]) + 1).ToArray<int>().Min());
+        }
+        Console.ReadLine();
+    }
+}
