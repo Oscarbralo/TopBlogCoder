d867bdb6f647e14d5bcd5838a1b8996d801beab8
Oscarbralo <oscarbralo@outlook.com>
2014-02-03 23:28:34 +0100

    New problems added

---
+++ SRMs/SRM211/DIV2/400/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _400
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            grafixCorrupt x = new grafixCorrupt();
+            string[] a = { "cat", "cab", "lab" };
+            Console.WriteLine(x.selectWord(a, "dab"));
+            Console.ReadLine();
+        }
+    }
+
+    public class grafixCorrupt
+    {
+        public int selectWord(string[] dictionary, string candidate)
+        {
+            int result = 0;
+            int resultMax = 0;
+            for (int i = 0; i < dictionary.Length; i++)
+            {
+                int j = -1;
+                int temp = dictionary[i].Where(x => dictionary[i][++j].ToString() == candidate[j].ToString()).Count();
+                if (temp > resultMax)
+                {
+                    resultMax = temp;
+                    result = i;
+                }
+            }
+            return result;
+        }
+    }
+}
