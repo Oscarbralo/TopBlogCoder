f238abf814d0035d8520ccc428228138957b4a43
Oscarbralo <oscarbralo@outlook.com>
2014-02-04 20:08:34 +0100

    New problems added

---
+++ Codeforces/ProblemALetsWatchFootball/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LetsWatchFootball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int temp = (input[0] * input[2]) - (input[1] * input[2]);
+            int seconds = 0;
+            while (seconds * input[1] < temp)
+                seconds++;
+            Console.WriteLine(seconds);
+            Console.ReadLine();
+        }
+    }
+}
