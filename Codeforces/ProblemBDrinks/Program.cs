2a783cbc1f942f8646808f48972b67cab1000e6a
Oscarbralo <oscarbralo@outlook.com>
2014-02-04 20:36:42 +0100

    New problems added

---
+++ Codeforces/ProblemBDrinks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Drinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            string res = (Console.ReadLine().Split(' ').Select(x => double.Parse(x)).ToArray<double>().Sum() / n).ToString("##.############").Replace(",", ".");
+            Console.WriteLine(string.IsNullOrEmpty(res) ? "0" : res);
+            Console.ReadLine();
+        }
+    }
+}
