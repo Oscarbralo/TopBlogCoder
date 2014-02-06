a5ea48b4f4a944a7e7f3c6f7609a1c23e978ccc3
Oscarbralo <oscarbralo@outlook.com>
2014-02-06 23:17:41 +0100

    new problem added

---
+++ Codeforces/ProblemABearAndRapsberry/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace BearAndRapsberry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cases = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int[] prices = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int result = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+                result = Math.Max(result, prices[i] - (prices[i + 1] + cases[1]));
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
