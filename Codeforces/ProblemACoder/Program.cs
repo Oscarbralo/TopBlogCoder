2581ab55da18e96f608b6eaddf268e8ebf50e84b
Oscarbralo <oscarbralo@outlook.com>
2014-02-07 22:52:29 +0100

    new problem added

---
+++ Codeforces/ProblemACoder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProblemACoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            string[] board = new string[cases];
+            int res = 0;
+            for (int i = 0; i < cases; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int a = 0; a < cases; a++)
+                    {
+                        board[i] += (a % 2 == 0) ? "C" : ".";
+                    }
+                    res += (cases + 1) / 2;
+                }
+                else
+                {
+                    for (int a = 0; a < cases; a++)
+                    {
+                        board[i] += (a % 2 == 1) ? "C" : ".";
+                    }
+                    res += cases / 2;
+                }
+            }
+            Console.WriteLine(res);
+            foreach(string b in board)
+                Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
