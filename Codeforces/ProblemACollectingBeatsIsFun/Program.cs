e4c157e4831a890490091bae5a93c7728182785b
Oscarbralo <oscarbralo@outlook.com>
2014-02-08 22:55:04 +0100

    new problem added

---
+++ Codeforces/ProblemACollectingBeatsIsFun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CollectingBeatIsFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hands = int.Parse(Console.ReadLine());
+            StringBuilder board = new StringBuilder();
+            for (int i = 0; i < 4; i++)
+                board.Append(Console.ReadLine());
+            bool finish = true;
+            for (int i = 1; i <= 9; i++)
+            {
+                if (board.ToString().Count(x => x.ToString() == i.ToString()) > hands * 2)
+                    finish = false;
+            }
+            if(finish)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            Console.ReadLine();
+        }
+    }
+}
