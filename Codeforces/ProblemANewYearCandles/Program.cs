3909699fda221dcbcd63494cb9b8ea11777338c9
Oscarbralo <oscarbralo@outlook.com>
2014-02-08 18:45:54 +0100

    new problem added

---
+++ Codeforces/ProblemANewYearCandles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace NewYearCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] candles = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int res = candles[0];
+            while (candles[0] >= candles[1])
+            {
+                res += candles[0] / candles[1];
+                candles[0] = (candles[0] % candles[1]) + (candles[0] / candles[1]);
+            }
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
