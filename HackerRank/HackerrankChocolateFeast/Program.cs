ad2001ca473e80b860b8175edf3117f4428962ad
Oscarbralo <oscarbralo@outlook.com>
2014-02-11 23:19:18 +0100

    new problem added

---
+++ HackerRank/HackerrankChocolateFeast/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int cases = int.Parse(Console.ReadLine());
+        for (int i = 0; i < cases; i++)
+        {
+            int[] temp = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int total = temp[0] / temp[1];
+            int wrappers = total;
+            while (wrappers >= temp[2])
+            {
+                int temp2 = wrappers / temp[2];
+                total += temp2;
+                wrappers = (wrappers - (temp2 * temp[2])) + temp2;
+            }
+            Console.WriteLine(total);
+        }
+        Console.ReadLine();
+    }
+}
