0a2080006f0294897acaea26ed27557ac22b3e0f
Oscarbralo <oscarbralo@outlook.com>
2014-02-15 20:48:54 +0100

    new problem added

---
+++ SRMs/SRM609/DIV2/250/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MagicalStringDiv2 x = new MagicalStringDiv2();
+            Console.WriteLine(x.minimalMoves(">><<><"));
+            Console.ReadLine();
+        }
+    }
+
+    public class MagicalStringDiv2
+    {
+        public int minimalMoves(string S)
+        {
+            string split1 = S.Substring(0, S.Length / 2);
+            string split2 = S.Substring(S.Length / 2, S.Length / 2);
+            int split1Left = split1.Count(x => x == '<');
+            int split2Right = split2.Count(x => x == '>');
+            return split1Left + split2Right;
+        }
+    }
+}
---
+++ SRMs/SRM609/DIV2/500/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PackingBallsDiv2 x = new PackingBallsDiv2();
+            Console.WriteLine(x.minPacks(0, 2, 1));
+            Console.ReadLine();
+        }
+    }
+
+    public class PackingBallsDiv2
+    {
+        public int minPacks(int R, int G, int B)
+        {
+            int boxes = 0;
+            while (R > 3)
+            {
+                R -= 3;
+                boxes++;
+            }
+            while (G > 3)
+            {
+                G -= 3;
+                boxes++;
+            }
+            while (B > 3)
+            {
+                B -= 3;
+                boxes++;
+            }
+            while (R > 0 || G > 0 || B > 0)
+            {
+                int count = 0;
+                if (R > 0)
+                {
+                    R--; 
+                    count++;
+                }
+                if (G > 0)
+                {
+                    G--;
+                    count++;
+                }
+                if (B > 0)
+                {
+                    B--;
+                    count++;
+                }
+                if (count == 1)
+                {
+                    if (R > 0)
+                    {
+                        R--;
+                        count++;
+                    }
+                    if (G > 0)
+                    {
+                        G--;
+                        count++;
+                    }
+                    if (B > 0)
+                    {
+                        B--;
+                        count++;
+                    }
+                }
+                boxes++;
+            }
+            return boxes;
+        }
+    }
+}
