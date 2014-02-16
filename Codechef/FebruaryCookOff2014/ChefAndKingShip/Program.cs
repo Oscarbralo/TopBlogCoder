d3669f6c87f6b7eed65de21db45def26d1174f4b
Oscarbralo <oscarbralo@outlook.com>
2014-02-16 20:24:34 +0100

    new problem added

---
+++ Codechef/FebruaryCookOff2014/ChefAndKingShip/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ChefAndKingShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int cities = int.Parse(Console.ReadLine());
+                int[] n = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+                long min = n.Min();
+                long total = n.Sum(x => x * min) - (min * min);
+                Console.WriteLine(total);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Codechef/FebruaryCookOff2014/ChefAndRemissness/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ChefAndRemissness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+                Console.WriteLine(Math.Max(input[0], input[1]) + " " + input.Sum());
+            }
+            Console.ReadLine();
+        }
+    }
+}
