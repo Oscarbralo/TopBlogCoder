3d3c0d5369ce2f5879ba872ff9c88f221a63fb25
Oscarbralo <oscarbralo@outlook.com>
2014-02-17 23:44:44 +0100

    new problem added

---
+++ SRMs/SRM224/DIV2/500/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoTurtledoves x = new TwoTurtledoves();
+            Console.WriteLine(x.presentType(12));
+            Console.ReadLine();
+        }
+    }
+
+    public class TwoTurtledoves
+    {
+        public int presentType(int n)
+        {
+            int a = 1;
+            int result = 0;
+            while (n > 0)
+            {
+                for (int i = a; i > 0; i--)
+                {
+                    for (int b = i; b > 0; b--)
+                    {
+                        n--;
+                        result = i;
+                        if (n == 0)
+                            break;
+                    }
+                    if (n == 0)
+                        break;
+                }
+                a++;
+            }
+            return result;
+        }
+    }
+}
