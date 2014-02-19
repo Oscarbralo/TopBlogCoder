0dd448d3def3d6644d60106b8cd3c7752b9dd695
Oscarbralo <oscarbralo@outlook.com>
2014-02-19 20:04:43 +0100

    new problem added

---
+++ SRMs/SRM227/DIV2/500/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientsList x = new ClientsList();
+            string[] a = { "Joe Smith", "Brown, Sam", "Miller, Judi" };
+            Console.WriteLine(x.dataCleanup(a));
+            Console.ReadLine();
+        }
+    }
+
+    public class ClientsList
+    {
+        public string[] dataCleanup(string[] names)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Contains(','))
+                {
+                    string[] t = names[i].Split(' ').Select(x => x.Replace(",", "")).ToArray<string>();
+                    string temp = t[1] + " " + t[0];
+                    list.Add(temp);
+                }
+                else
+                    list.Add(names[i]);
+            }
+            return list.OrderBy(x => x.Split(' ')[1]).ThenBy(x => x.Split(' ')[0]).ToArray<string>();
+        }
+    }
+}
