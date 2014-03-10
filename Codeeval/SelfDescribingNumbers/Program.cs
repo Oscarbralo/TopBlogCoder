09a002048dac3bb5ce926f3cf24619a7a69c3fd9
Oscarbralo <oscarbralo@outlook.com>
2014-03-10 23:29:18 +0100

    new problem added

---
+++ Codeeval/SelfDescribingNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace SelfDescribingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "2020";
+            bool finish = true;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (int.Parse(line[i].ToString()) != line.Count(x => x.ToString() == i.ToString()))
+                {
+                    finish = false;
+                    break;
+                }
+            }
+            Console.WriteLine(finish == true ? 1 : 0);
+            Console.ReadLine();
+        }
+    }
+}
