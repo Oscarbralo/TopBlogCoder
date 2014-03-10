07e2bfc9cce7305c2300d54afc7d7a664e092109
Oscarbralo <oscarbralo@outlook.com>
2014-03-10 23:12:37 +0100

    new problem added

---
+++ Codeeval/LongestWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] temp = line.Split(' ');
+                string result = string.Empty;
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    if (temp[i].Length > result.Length)
+                    {
+                        result = temp[i];
+                    }
+                }
+                Console.WriteLine(result);
+            }
+    }
+}
