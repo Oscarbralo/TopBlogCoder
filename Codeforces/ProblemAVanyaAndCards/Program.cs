c7e8ac3893c00cd4b4c51ef6aa1abe486db3dbbd
Oscarbralo <oscarbralo@outlook.com>
2014-03-10 20:08:52 +0100

    new problem added

---
+++ Codeforces/ProblemAVanyaAndCards/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace VanyaAndCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cases = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int sum = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).Sum();
+            int total = Math.Abs(sum * -1);
+            Console.WriteLine(total % cases[1] > 0 ? total / cases[1] + 1 : total / cases[1]);
+            Console.ReadLine();
+        }
+    }
+}
