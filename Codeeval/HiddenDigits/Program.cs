3f0d6c41d09807e75e2a9e8865822fdccf37ed3c
Oscarbralo <oscarbralo@outlook.com>
2014-03-12 19:42:11 +0100

    new problem added

---
+++ Codeeval/HiddenDigits/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace HiddenDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "abcdefghik";
+            string abc = "abcdefghij";
+            string num = "0123456789";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (abc.Contains(line[i]))
+                    sb.Append(num[abc.IndexOf(line[i])]);
+                else if (num.Contains(line[i]))
+                    sb.Append(line[i]);
+            }
+            Console.WriteLine(string.IsNullOrEmpty(sb.ToString()) ? "NONE" : sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
