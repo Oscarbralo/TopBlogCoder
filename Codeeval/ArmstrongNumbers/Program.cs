da1bc1d5931864b521cd283a47416ad4dc86d893
Oscarbralo <oscarbralo@outlook.com>
2014-03-13 18:46:13 +0100

    new problem added

---
+++ Codeeval/ArmstrongNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ArmstrongNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cases = "153";
+            int res = 0;
+            cases.Select(x => x.ToString()).ToList().ForEach(j => res += (int)Math.Pow(double.Parse(j.ToString()), double.Parse(cases.Length.ToString())));
+            Console.ReadLine();
+        }
+    }
+}
