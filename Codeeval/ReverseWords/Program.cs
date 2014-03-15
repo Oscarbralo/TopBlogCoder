319c516b6a9ec58292da00e0b87683a8ff472e36
Oscarbralo <oscarbralo@outlook.com>
2014-03-15 20:08:58 +0100

    new problem added

---
+++ Codeeval/ReverseWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ReverseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Hello World";
+            Console.WriteLine(string.Join(" ", line.Split(' ').Reverse().ToArray<string>()));
+            Console.ReadLine();
+        }
+    }
+}
