f68ecdc84b266c100db2a89ef090708b776100c3
Oscarbralo <oscarbralo@outlook.com>
2014-03-15 19:34:12 +0100

    new problem added

---
+++ Codeeval/JsonIds/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace JsonIds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "{'menu'";
+            Regex r = new Regex(@"(Label )\d+");
+            int result = 0;
+            foreach (Match m in r.Matches(line))
+                result += int.Parse(m.Value.Split(' ')[1]);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
