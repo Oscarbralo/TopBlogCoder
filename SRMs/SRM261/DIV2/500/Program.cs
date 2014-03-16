1bf84e861a8fe4b868544ac6b032f46fb3d7097d
Oscarbralo <oscarbralo@outlook.com>
2014-03-16 21:11:14 +0100

    new problem added

--- SRMs/SRM261/DIV2/500/Program.cs
+++ SRMs/SRM261/DIV2/500/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace _500
 {
@@ -9,6 +10,9 @@ namespace _500
     {
         static void Main(string[] args)
         {
+            PrimeStatistics x = new PrimeStatistics();
+            Console.WriteLine(x.mostCommonRemainder(3, 14, 5));
+            Console.ReadLine();
         }
     }
 
@@ -16,43 +20,30 @@ namespace _500
     {
         public int mostCommonRemainder(int lowerBound, int upperBound, int modulo)
         {
-            List<int> reminders = new List<int>();
-            List<int> times = new List<int>();
-            for (int i = lowerBound; i <= upperBound; i++)
+            List<int> list = GetPrimes(lowerBound, upperBound).Select(x => x %= modulo).ToList();
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < list.Count; i++)
             {
-                if (isPrime(i) == true)
-                {
-                    int rem = i % modulo;
-                    if (!reminders.Contains(rem))
-                    {
-                        reminders.Add(rem);
-                        times.Add(1);
-                    }
-                    else
-                        times[reminders.IndexOf(rem)]++;
-                }
+                if (!dic.ContainsKey(list[i]))
+                    dic.Add(list[i], 1);
+                else
+                    dic[list[i]] += 1;
             }
-            int result = 1000000;
-            int resTimes = 0;
-            for (int i = 0; i < times.Count; i++)
-            {
-                if (times[i] > resTimes)
-                {
-                    result = reminders[i];
-                    resTimes = times[i];
-                }
-                else if (times[i] == resTimes && reminders[i] < result)
-                {
-                    result = reminders[i];
-                    resTimes = times[i];
-                }
-            }
-            if (result == 1000000)
-                return 0;
-            return result;
+            return dic.OrderByDescending(x => x.Value).ThenBy(x => x.Key).Select(x => x.Key).FirstOrDefault();
         }
 
-        public bool isPrime(int n)
+        public List<int> GetPrimes(int lowerBound, int upperDound)
+        {
+            List<int> list = new List<int>();
+            for (int i = lowerBound; i <= upperDound; i++)
+            {
+                if (IsPrime(i))
+                    list.Add(i);
+            }
+            return list;
+        }
+
+        public bool IsPrime(int n)
         {
             if (n <= 1) return false;
             if (n == 2) return true;
@@ -60,9 +51,7 @@ namespace _500
             int m = (int)Math.Sqrt(n);
 
             for (int i = 3; i <= m; i += 2)
-                if (n % i == 0)
-                    return false;
-
+                if (n % i == 0) return false;
             return true;
         }
     }
