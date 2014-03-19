6dbc000ccbf7e74f3a335ed5e126ca3992716f22
Oscarbralo <oscarbralo@outlook.com>
2014-03-19 22:40:50 +0100

    new problem added

---
+++ Codeeval/NumberOfOnes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                int n = int.Parse(reader.ReadLine());
+                Console.WriteLine(Convert.ToString(n, 2).Count(x => x == '1'));
+            }
+    }
+}
