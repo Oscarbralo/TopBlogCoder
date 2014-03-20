a93b2dcd72a79b1d3c6c9efccdb464e42955d028
Oscarbralo <oscarbralo@outlook.com>
2014-03-20 23:13:39 +0100

    new problem added

---
+++ Codeeval/FirstNonRepeatedCharacter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace FirstNonRdCharacterepeate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "tooth";
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line.Where(x => x == line[i]).Count() == 1)
+                {
+                    Console.WriteLine(line[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
