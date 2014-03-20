f058dfc68d10e8c9b53cf357df8f4ebb59ec596e
Oscarbralo <oscarbralo@outlook.com>
2014-03-20 20:15:33 +0100

    new problem added

---
+++ Codeeval/StringRotation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {
+                string[] line = reader.ReadLine().Split(',');
+                bool finish = false;
+                for (int i = 0; i < line[0].Length; i++)
+                {
+                    string temp = string.Join("", line[0].Skip(i).Take(line[0].Length - i).Select(x => x.ToString()).ToArray<string>()) + string.Join("", line[0].Take(i).Select(x => x.ToString()).ToArray<string>());
+                    if (temp == line[1])
+                    {
+                        finish = true;
+                        break;
+                    }
+                }
+                Console.WriteLine(finish ? "True" : "False");
+            }
+    }
+}
