68d28b7b1980b4374f00ca4f86641974012ac9d2
Oscarbralo <oscarbralo@outlook.com>
2014-03-22 19:56:59 +0100

    new problem added

---
+++ Codeforces/ProblemAGravityFlip/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace GravityFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(string.Join(" ", Console.ReadLine().Split(' ').Select(x => int.Parse(x)).
+                OrderBy(x => x).Select(x => x.ToString()).ToArray<string>()));
+            Console.ReadLine();
+        }
+    }
+}
