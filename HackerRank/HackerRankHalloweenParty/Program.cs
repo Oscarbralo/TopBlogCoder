819442ae230fb1eb7a259617eb80e5f2b211ce6c
Oscarbralo <oscarbralo@outlook.com>
2014-03-24 19:58:29 +0100

    new problem added

---
+++ HackerRank/HackerRankHalloweenParty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int cases = int.Parse(Console.ReadLine());
+        for (int i = 0; i < cases; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long a1 = n / 2;
+            long a2 = (n + 1) / 2;
+            long temp = (long)Math.Pow(n / 2, 2);
+            long result = n % 2 == 0 ? temp : a2 * a1;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HackerRank/HackerRankIsFibo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int caseNumber = int.Parse(Console.ReadLine());
+        for (int i = 0; i < caseNumber; i++)
+        {
+            long cases = long.Parse(Console.ReadLine());
+            List<long> list = new List<long>();
+            list.Add(1);
+            long n = list[list.Count - 1];
+            while (n < cases)
+            {
+                long temp = list[list.Count - 1];
+                list.Add(n + temp);
+                n = temp;
+            }
+            Console.WriteLine(n == cases ? "IsFibo" : "IsNotFibo");
+        }
+    }
+}
