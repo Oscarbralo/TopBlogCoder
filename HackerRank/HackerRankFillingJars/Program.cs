e45b53e62b08468a3a910b2ac03d2729e8218e40
Oscarbralo <oscarbralo@outlook.com>
2014-03-24 23:14:41 +0100

    new problem added

---
+++ HackerRank/HackerRankFillingJars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        long[] temp = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray<long>();
+        long res = 0;
+        for (int i = 0; i < temp[1]; i++)
+        {
+            long[] input = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray<long>();
+            res += ((input[1] + 1) - input[0]) * input[2];
+        }
+        double result = res / temp[0];
+        res = (long)Math.Floor(result);
+        Console.WriteLine(res);
+    }
+}
