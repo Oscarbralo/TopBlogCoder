5b9cef3b64f80c40617dfc89a8e32c2977342f35
Oscarbralo <oscarbralo@outlook.com>
2014-03-30 19:15:12 +0200

    new problem added

---
+++ Codeforces/ProblemALineToCashiers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LineToCashiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cashes = int.Parse(Console.ReadLine());
+            int[] numberItems = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int min = int.MaxValue;
+            for (int i = 0; i < cashes; i++)
+            {
+                List<int> items = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList<int>();
+                int result = 0;
+                items.ForEach(x => result += x * 5);
+                result += items.Count * 15;
+                min = Math.Min(result, min);
+            }
+            Console.WriteLine(min);
+            Console.ReadLine();
+        }
+    }
+}
