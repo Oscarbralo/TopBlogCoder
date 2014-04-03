6b64788ff6d06df0ea6485110f03a7df6bcf83d2
Oscarbralo <oscarbralo@outlook.com>
2014-04-03 23:08:58 +0200

    new problem added

---
+++ Codechef/Buy1Get1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Buy1Get1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int result = 0;
+                int[] times = new int[52];
+                Console.ReadLine().Select(x => x.ToString()).ToList<string>().ForEach(x => times[abc.IndexOf(x)] += 1);
+                times.ToList().ForEach(x => result += (x + 1) / 2);
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
