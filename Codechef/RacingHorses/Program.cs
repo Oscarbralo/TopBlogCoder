b2be417f2704c0407fcba922cbb99f33a015020a
Oscarbralo <oscarbralo@outlook.com>
2014-04-06 23:05:33 +0200

    new problem added

---
+++ Codechef/RacingHorses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace RacingHorses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                long[] horses = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).OrderBy(x => x).ToArray<long>();
+                long min = long.MaxValue;
+                for (int a = 0; a < horses.Length - 1; a++)
+                {
+                        min = Math.Min(horses[a - 1] - horses[a], min);
+                }
+                Console.WriteLine(min);
+            }
+            Console.ReadLine();
+        }
+    }
+}
