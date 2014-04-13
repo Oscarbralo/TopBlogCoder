dd706e3529273dc8c98aa4cf316ec81803af34fe
Oscarbralo <oscarbralo@outlook.com>
2014-04-13 07:40:01 +0200

    new problem added

---
+++ GoogleCodeJam/GCJ14/ProblemA/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\GCJ\A-small-attempt0.in");
+            StreamWriter sw = new StreamWriter(@"C:\GCJ\A-Output.txt");
+            string line = string.Empty;
+            int cases = int.Parse(sr.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int answerFirst = int.Parse(sr.ReadLine()) - 1;
+                string[] firstBoard = new string[4];
+                for (int a = 0; a < 4; a++)
+                {
+                    firstBoard[a] = sr.ReadLine();
+                }
+                int secondFirst = int.Parse(sr.ReadLine()) - 1;
+                string[] secondBoard = new string[4];
+                for (int a = 0; a < 4; a++)
+                {
+                    secondBoard[a] = sr.ReadLine();
+                }
+                List<int> list = new List<int>();
+                int[] firstRow = firstBoard[answerFirst].Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+                int[] secondRow = secondBoard[secondFirst].Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+                for (int a = 0; a < firstRow.Length; a++)
+                {
+                    if (secondRow.Contains(firstRow[a]))
+                        list.Add(firstRow[a]);
+                }
+                if (list.Count == 0)
+                    sw.WriteLine(string.Format("Case #{0}: {1}", i + 1, "Volunteer cheated!"));
+                if (list.Count == 1)
+                    sw.WriteLine(string.Format("Case #{0}: {1}", i + 1, list[0]));
+                if (list.Count > 1)
+                    sw.WriteLine(string.Format("Case #{0}: {1}", i + 1, "Bad magician!"));
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
---
+++ GoogleCodeJam/GCJ14/ProblemB/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.IO;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\GCJ\B-large.in");
+            StreamWriter sw = new StreamWriter(@"C:\GCJ\B-OutputLarge.txt");
+            string line = string.Empty;
+            int cases = int.Parse(sr.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                double cookiesPerSecond = 2.0;
+                string[] text = sr.ReadLine().Split(' ').Select(x => x.Replace('.', ',')).ToArray<string>();
+                double[] input = new double[3];
+                for (int a = 0; a < text.Length; a++)
+                    input[a] = Convert.ToDouble(text[a]);
+                double costOfFarm = input[0];
+                double totalTempTime = 0.0;
+                List<double> times = new List<double>();
+                double farmsLeft = input[2] / costOfFarm;
+                for (int a = 0; a < 200000; a++)
+                {
+                    double timeToFarm = costOfFarm / cookiesPerSecond;
+                    times.Add(totalTempTime + (timeToFarm * farmsLeft));
+                    totalTempTime += timeToFarm;
+                    double leftTimeCookies = (input[2] / cookiesPerSecond) + totalTempTime;
+                    times.Add(leftTimeCookies);
+                    cookiesPerSecond += input[1];
+                }
+                string result = Convert.ToString(times.Min()).Replace(',', '.');
+                sw.WriteLine(string.Format("Case #{0}: {1}", i + 1, result));
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
