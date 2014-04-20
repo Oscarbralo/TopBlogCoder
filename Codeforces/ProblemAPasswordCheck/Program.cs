e98be4e0d8189861111e4d93ea6a2922f95832af
Oscarbralo <oscarbralo@outlook.com>
2014-04-20 18:02:59 +0200

    new problem added

---
+++ Codeforces/ProblemAPasswordCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ProblemAPasswordCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string abc = "abcdefghijklmnopqrstuvwxyz";
+            string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string numbers = "0123456789";
+            if (text.Any(x => abc.Contains(x)) && text.Any(x => ABC.Contains(x)) && text.Any(x => numbers.Contains(x)) && text.Length >= 5)
+                Console.WriteLine("Correct");
+            else
+                Console.WriteLine("Too weak");
+            Console.ReadLine();
+        }
+    }
+}
