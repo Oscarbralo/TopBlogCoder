a123271fbf05feac75ccb1a9c3b201b1e2adfb1e
Oscarbralo <oscarbralo@outlook.com>
2014-05-12 19:53:58 +0200

    new problem added

---
+++ Codechef/MayChallenge2014/OJUMPS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace ChefJumping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = long.Parse(Console.ReadLine());
+            long x = a % 6;
+            if (x == 0 || x == 1 || x == 3)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Codechef/MayChallenge2014/RRSTONE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace StackOverFlow
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            long[] n = Console.ReadLine().Split(' ').Select(x => long.Parse(x)).ToArray();
+            if (input[1] == 0)
+                Console.WriteLine(string.Join(" ", n.Select(x => x.ToString()).ToArray()));
+            else
+            {
+                long[] newN = new long[n.Length];
+                long max = n.Max();
+                for (int i = 0; i < n.Length; i++)
+                    n[i] = max - n[i];
+                long max2 = n.Max();
+                for (int i = 0; i < n.Length; i++)
+                    newN[i] = max2 - n[i];
+                if (input[1] % 2 == 0)
+                    Console.WriteLine(string.Join(" ", newN.Select(x => x.ToString()).ToArray()));
+                else
+                    Console.WriteLine(string.Join(" ", n.Select(x => x.ToString()).ToArray()));
+            }
+            Console.ReadLine();
+        }
+    }
+}
