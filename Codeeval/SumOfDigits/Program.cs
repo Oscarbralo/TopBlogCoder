98d4319c3be549559149b9542bba50cf76335b41
Oscarbralo <oscarbralo@outlook.com>
2014-05-15 09:49:30 +0200

    new problem added

---
+++ Codeeval/SumOfDigits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine().Select(x => int.Parse(x.ToString())).Sum());
+                }
+            Console.ReadLine();
+        }
+    }
+}
