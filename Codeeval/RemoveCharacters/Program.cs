ea0e3fe0b7788a519f4a11980594da48f2f59c22
Oscarbralo <oscarbralo@outlook.com>
2014-05-17 17:50:28 +0200

    new problem added

---
+++ Codeeval/RemoveCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace RemoveCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string[] input = reader.ReadLine().Split(',');
+                    input[1].Trim().ToList().ForEach(x => input[0] = input[0].Replace(x.ToString(), ""));
+                    Console.WriteLine(input[0]);
+                }
+            Console.ReadLine();
+        }
+    }
+}
