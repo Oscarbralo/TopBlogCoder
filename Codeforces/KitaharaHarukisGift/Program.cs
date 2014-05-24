5bcdbb30e200ad69590120c7d3a968cb55d1eb39
Oscarbralo <oscarbralo@outlook.com>
2014-05-24 16:47:20 +0200

    new problem added

---
+++ Codeforces/KitaharaHarukisGift/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace KitaharaHarukisGift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int apples = int.Parse(Console.ReadLine());
+            int[] grams = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).OrderByDescending(x => x).ToArray<int>();
+            int friend1 = 0;
+            int friend2 = 0;
+            for (int i = 0; i < grams.Length; i++)
+            {
+                if (friend1 <= friend2)
+                    friend1 += grams[i];
+                else
+                    friend2 += grams[i];
+            }
+            Console.WriteLine(friend1 == friend2 ? "YES" : "NO");
+            Console.ReadLine();
+        }
+    }
+}
