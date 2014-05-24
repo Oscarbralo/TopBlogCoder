7a083ceb5f608904843f88ec93d91bf3c3cc7149
Oscarbralo <oscarbralo@outlook.com>
2014-05-24 16:22:15 +0200

    new problem added

---
+++ HackerRank/SherlockAndTheBeast/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+
+namespace SherlockAndTheBeast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string three = "555";
+            string five = "33333";
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                StringBuilder sb = new StringBuilder();
+                int res = n / 3;
+                while (res > 0)
+                {
+                    sb.Append(three);
+                    res--;
+                }
+                int resFinal = n % 3;
+                while (resFinal != 0 && sb.Length > 0)
+                {
+                        sb = sb.Remove(0, 3);
+                        resFinal += 3;
+                        if (resFinal % 5 == 0)
+                            break;
+                }
+                if (resFinal % 5 == 0)
+                {
+                    while (resFinal > 0)
+                    {
+                        sb.Insert(sb.Length, five);
+                        resFinal -= 5;
+                    }
+                    Console.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    Console.WriteLine(-1);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
