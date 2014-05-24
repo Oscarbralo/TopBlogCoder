aa80f3a3d7258e2db4e2bc00e84dd5e6bbc7a225
Oscarbralo <oscarbralo@outlook.com>
2014-05-24 17:33:09 +0200

    new problem added

---
+++ Codeeval/SumOfIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SumOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (StreamReader reader = File.OpenText(args[0]))
+            //    while (!reader.EndOfStream)
+            //    {
+                    int[] numbers = Console.ReadLine().Split(',').Select(x => int.Parse(x.Trim())).ToArray<int>();
+                    int bigSum = int.MaxValue * -1;
+                    for (int i = 0; i < numbers.Length - 1; i++)
+                    {
+                        for (int a = numbers.Length; a > i; a--)
+                        {
+                            bigSum = Math.Max(numbers.Skip(i).Take(a - i).Sum(), bigSum);
+                        }
+                    }
+                    Console.WriteLine(bigSum == int.MaxValue * -1 ? numbers[0] : bigSum);
+                //}
+            Console.ReadLine();
+        }
+    }
+}
