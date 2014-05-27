9a32ebc7f1efd190a3c46fc9d26a0199928e4f84
Oscarbralo <oscarbralo@outlook.com>
2014-05-27 22:15:38 +0200

    new problem added

---
+++ Codeforces/BlackSquare/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace BlackSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int[] n = Console.ReadLine().Select(x => int.Parse(x.ToString()) - 1).ToArray<int>();
+            long result = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                result += numbers[n[i]];
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
