ee475538f5fcef2adacf6cf1f5c1ea100bd50aaf
Oscarbralo <oscarbralo@outlook.com>
2014-05-27 22:52:14 +0200

    new problem added

---
+++ Codechef/YetAnotherNumberGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace YetAnotherNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(n % 2 == 1 ? "BOB" : "ALICE");
+            }
+            Console.ReadLine();
+        }
+    }
+}
