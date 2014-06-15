f72b316c536997ae27ee379a4a855299169dad41
Oscarbralo <oscarbralo@outlook.com>
2014-06-15 19:19:24 +0200

    new problem added

---
+++ Codeforces/ValeraAndAntiqueItems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ValeraAndAntiquesItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cases = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            List<int> results = new List<int>();
+            for (int i = 0; i < cases[0]; i++)
+            {
+                int[] prices = Console.ReadLine().Split(' ').Skip(1).Select(x => int.Parse(x)).ToArray<int>();
+                if (prices.Any(x => cases[1] > x))
+                    results.Add(i + 1);
+            }
+            Console.WriteLine(results.Count);
+            Console.WriteLine(string.Join(" ", results.Select(x => x.ToString()).ToArray<string>()));
+            Console.ReadLine();
+        }
+    }
+}
