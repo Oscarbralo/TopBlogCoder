fbae3a885fbb56f143b594651717f0230d7e2069
Oscarbralo <oscarbralo@outlook.com>
2014-06-15 16:51:20 +0200

    new problem added

---
+++ Codeforces/ForgottenEpisode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ForgottenEpisode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxEpisodes = int.Parse(Console.ReadLine());
+            Console.WriteLine(Enumerable.Range(1, maxEpisodes).Except(Console.ReadLine().Split(' ').Select(x => int.Parse(x))).ToList()[0]);
+        }
+    }
+}
