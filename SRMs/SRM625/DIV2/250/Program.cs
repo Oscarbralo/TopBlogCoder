df138965ee310689dfc1f9b240f9c657955a529e
Oscarbralo <oscarbralo@outlook.com>
2014-06-19 16:42:25 +0200

    new problems added

---
+++ SRMs/SRM625/DIV2/250/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _250
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddMultiply x = new AddMultiply();
+            Console.WriteLine(x.makeExpression(2));
+            Console.ReadLine();
+        }
+    }
+
+    public class AddMultiply
+    {
+        public int[] makeExpression(int y)
+        {
+            int temp1 = 0;
+            int temp2 = 2;
+            int temp3 = 0;
+            if (y == 0)
+            {
+                temp1 = 2;
+                temp2 = 2;
+                temp3 = 4;
+            }
+            else if (y == 1)
+            {
+                temp1 = 2;
+                temp2 = 2;
+                temp3 = 3;
+            }
+            else
+            {
+                temp1 = y / 2;
+                while ((temp1 * temp2) - y <= 1)
+                {
+                    temp1++;
+                }
+                temp3 = (temp1 * temp2) - y;
+            }
+            int[] result = { temp1, temp2, temp3 * -1 };
+            return result;
+        }
+    }
+}
---
+++ SRMs/SRM625/DIV2/500/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace _500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IncrementingSequence x = new IncrementingSequence();
+            int[] a = { 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2 };
+            Console.WriteLine(x.canItBeDone(2, a));
+            Console.ReadLine();
+        }
+    }
+
+    public class IncrementingSequence
+    {
+        public string canItBeDone(int k, int[] A)
+        {
+            A = A.OrderBy(x => x).ToArray<int>();
+            int[] temp = Enumerable.Range(1, A.Length).ToArray<int>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] < temp[i])
+                {
+                    while (A[i] < temp[i])
+                    {
+                        A[i] += k;
+                    }
+                    if (A[i] > temp[i])
+                    {
+                        A = A.OrderBy(x => x).ToArray<int>();
+                        i--;
+                    }
+                }
+            }
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] != temp[i])
+                    return "IMPOSSIBLE";
+            }
+            return "POSSIBLE";
+        }
+    }
+}
